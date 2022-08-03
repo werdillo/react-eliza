5ccf1f17733213e32c6ba6becf72aae131f1e587
Alexey Tihomirov <wediktivus@gmail.com>
2022-08-04 00:21:03 +0300

    Updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,19 +3,26 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { Row, Col, Container } from 'react-bootstrap';
 import logo from "../assets/images/footer/Logo.svg";
+
 import facebook from "../assets/images/footer/facebook-icon.svg";
 import instagram from "../assets/images/footer/instagram-icon.svg";
 import youtube from "../assets/images/footer/youtube-icon.svg";
+
+import Special2017 from "../assets/images/footer/Special2017.svg";
 import Winner2018 from "../assets/images/footer/Winner2018.svg";
 import Winner2020 from "../assets/images/footer/Winner2020.svg";
 import InnovationAward from "../assets/images/footer/InnovationAward.svg";
+import IconicAwards2018 from "../assets/images/footer/IconicAwards2018.svg";
+import DesignAwards2016 from "../assets/images/footer/DesignAwards2016.svg";
+import InteriorAwards2015 from "../assets/images/footer/InteriorAwards2015.svg";
 import IfAward2018 from "../assets/images/footer/IfAward2018.svg";
+import IfAward2020 from "../assets/images/footer/IfAward2020.svg";
 
 export default function Footer() {
 	return (
 		<Container fluid className='footer-container'>
 			<Row>
-				<Col sm={12} lg={3}>
+				<Col sm={12} lg={1}>
 					<div className='footer-left'>
 						<div className='footer-left-wrapper'>
 							<img src={logo} alt="" className='footer-logo'></img>
@@ -27,20 +34,17 @@ export default function Footer() {
 						</div>
 					</div>
 				</Col>
-				<Col sm={12} lg={6}>
+				<Col sm={12} lg={11}>
 					<div className='footer-awards'>
+						<img src={InteriorAwards2015} alt="" className='footer-img'></img>
+						<img src={DesignAwards2016} alt="" className='footer-img'></img>
+						<img src={IconicAwards2018} alt="" className='footer-img'></img>
+						<img src={Special2017} alt="" className='footer-img'></img>
 						<img src={Winner2018} alt="" className='footer-img'></img>
-						<img src={Winner2020} alt="" className='footer-img'></img>
-						<img src={InnovationAward} alt="" className='footer-img'></img>
 						<img src={IfAward2018} alt="" className='footer-img'></img>
-					</div>
-				</Col>
-				<Col sm={12} lg={3}>
-					<div className='footer-text-wrapper'>
-						<p className='footer-text'>+371 66 555 040</p>
-						<p className='footer-text'>Riga, A. Deglava iela 50,</p>
-						<p className='footer-text'>LV-1035</p>
-						<p className='footer-text'>info@eliza-k.lv</p>
+						<img src={Winner2020} alt="" className='footer-img'></img>
+						<img src={IfAward2020} alt="" className='footer-img'></img>
+						<img src={InnovationAward} alt="" className='footer-img'></img>
 					</div>
 				</Col>
 				{/* Eliza-K year */}
