087ee718e1a9ac9abfb67f3f55aa8f2718e6d0e3
Alexey Tihomirov <wediktivus@gmail.com>
2022-08-29 01:47:20 +0300

    video player

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
+
 import '../App.css';
 import Scene1 from "../assets/images/AboutUs/Scene1.png";
 import Scene2 from "../assets/images/AboutUs/Scene2.png";
@@ -16,6 +18,18 @@ function AboutUsItem(props) {
 export default function AboutUs() {
 	return (
 		<>
+			<div className='player-wrap'>
+				<div className='react-player-item'>
+					<ReactPlayer
+						className='react-player'
+						url='video/demo_video.MOV'
+						width="100%"
+						height="auto"
+						controls={true}
+						playing={true}
+					/>
+				</div>
+			</div>
 			<img src={Scene1} alt='' className='map-image'></img>
 			<AboutUsItem img={Scene2} text="Eliza produkcija ik gadu saņem lielu atzinību starptautiskajās izstādēs, kā arī veiksmīgi eksportē savu produkciju uz Eiropas valstīm." />
 			<AboutUsItem img={Scene3} text="Piedāvājam saviem pircējiem iespēju veikt nestandarta pasūtījumus. Šādu pasūtījumu veikšanas termiņš ir 1 - 2 mēneši." />
