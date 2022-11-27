59de2305421140c04e4b39479e122dd1de60056a
werdillo <55383654+werdillo@users.noreply.github.com>
2022-11-27 11:15:41 +0200

    change filter style

--- src/Components/ProductionItem.js
+++ src/Components/ProductionItem.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
-// import { useNavigate } from 'react-router-dom';
-
 import '../App.css';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { Row, Col, Container } from 'react-bootstrap';
 
-
-import size from "../assets/images/production/size.svg";
-import headboard from "../assets/images/production/headboard.svg";
-import options from "../assets/images/production/options.svg";
-import cleaning from "../assets/images/production/cleaning.svg";
 import cleaningFile from "../assets/Files/Products/cleaning.pdf";
 
 // swiper
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react'
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import { Navigation, Autoplay, Pagination } from "swiper";
 import 'swiper/swiper.min.css'
 import 'swiper/modules/pagination/pagination.min.css'
 import 'swiper/modules/navigation/navigation.min.css'
 
 export default function ProductionItem(props) {
-	// const navigate = useNavigate();
 	return (
 		<>
 			<Swiper
@@ -50,7 +42,6 @@ export default function ProductionItem(props) {
 						<p className='item-text'>{props.description}</p>
 						{props.spec ?
 							<div className='item-specs-block'>
-								{/* return spec */}
 								{props.specs.map((spec) =>{
 									return (
 										<div className='item-spec'>
@@ -67,36 +58,26 @@ export default function ProductionItem(props) {
 							{props.file ?
 								<a href={props.file} target='_blank' rel='noopener noreferrer'>
 									<div className='item-link'>Specification</div>
-									{/* <img src={size} alt="" className="item-document-link" /> */}
 								</a>
 
 								: null}
 							{props.file_headboard ?
 								<a href={props.file_headboard} target='_blank' rel='noopener noreferrer'>
 									<div className='item-link'>Headboard</div>
-									{/* <img src={headboard} alt="" className="item-document-link" /> */}
 								</a>
 
 								: null}
 							{props.file_options ?
 								<a href={props.file_options} target='_blank' rel='noopener noreferrer'>
 									<div className='item-link'>Options</div>
-									{/* <img src={options} alt="" className="item-document-link" /> */}
 								</a>
 
 								: null}
 							<a href={cleaningFile} target='_blank' rel='noopener noreferrer'>
 								<div className='item-link'>Cleaning</div>
-
-								{/* <img src={cleaning} alt="" className="item-document-link" /> */}
 							</a>
 						</div>
-						
 					</Col>
-					{/* <Col sm={12} lg={12}>
-						<div className="item-back-button" onClick={() => navigate(-1)}>Back</div>
-					</Col> */}
-
 
 				</Row>
 			</Container>
