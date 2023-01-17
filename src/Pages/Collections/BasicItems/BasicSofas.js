20902ed959e3b4c7eb0749a1ea969703e422f7cb
werdillo <55383654+werdillo@users.noreply.github.com>
2023-01-17 16:19:27 +0200

    new items in bbold and basic

--- src/Pages/Collections/BasicItems/BasicSofas.js
+++ src/Pages/Collections/BasicItems/BasicSofas.js
@@ -1,14 +1,26 @@
 const Basic_sofa = [
 	{
 		id: 1,
-		path: "sofa/altitude",
-		title: "Altitude",
-		file: require("../../../assets/Files/collection/bbold/Altitude.pdf"),
+		path: "sofa/boston",
+		title: "Boston",
+		file: require("../../../assets/Files/collection/basic/Boston.pdf"),
 		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/altitude/altitude_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/altitude/altitude_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/altitude/altitude_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/altitude/altitude_4.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/boston/boston_1.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/boston/boston_2.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/boston/boston_3.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/boston/boston_4.jpg"),
+		]
+	},
+	{
+		id: 2,
+		path: "sofa/astorija",
+		title: "Astorija",
+		file: require("../../../assets/Files/collection/basic/Astorija.pdf"),
+		photo: [
+			require("../../../assets/images/collection/basic/Sofas/astorija/astorija_1.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/astorija/astorija_2.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/astorija/astorija_3.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/astorija/astorija_4.jpg"),
 		]
 	},
 ]
--- src/Pages/Collections/BboldItems/BboldSofas.js
+++ src/Pages/Collections/BboldItems/BboldSofas.js
@@ -11,6 +11,30 @@ const Bbold_sofa = [
 			require("../../../assets/images/collection/bbold/Sofas/altitude/altitude_4.jpg"),
 		]
 	},
+	{
+		id: 2,
+		path: "sofa/shetland",
+		title: "Shetland",
+		file: require("../../../assets/Files/collection/bbold/Shetland.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Sofas/shetland/shetland_1.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/shetland/shetland_2.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/shetland/shetland_3.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/shetland/shetland_4.jpg"),
+		]
+	},
+	{
+		id: 3,
+		path: "sofa/box",
+		title: "Box",
+		file: require("../../../assets/Files/collection/bbold/Box.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Sofas/box/box_1.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/box/box_2.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/box/box_3.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/box/box_4.jpg"),
+		]
+	},
 ]
 
 export default Bbold_sofa;
