484550476b05e90e01afa031a8a34f5224582147
werdillo <wediktivus@gmail.com>
2023-02-28 14:21:50 +0200

    changed sofas location

--- src/Pages/Collections/BasicItems/BasicSofas.js
+++ src/Pages/Collections/BasicItems/BasicSofas.js
@@ -23,6 +23,42 @@ const Basic_sofa = [
 			require("../../../assets/images/collection/basic/Sofas/boston/boston_4.jpg"),
 		]
 	},
+	{
+		id: 3,
+		path: "sofa/lusso",
+		title: "Lusso",
+		file: require("../../../assets/Files/collection/bbold/Lusso.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_1.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_2.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_3.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_4.jpg"),
+		]
+	},
+	{
+		id: 4,
+		path: "sofa/richard",
+		title: "Richard",
+		file: require("../../../assets/Files/collection/bbold/Richard.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Sofas/richard/richard_1.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/richard/richard_2.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/richard/richard_3.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/richard/richard_4.jpg"),
+		]
+	},
+	{
+		id: 5,
+		path: "sofa/argo",
+		title: "Argo",
+		file: require("../../../assets/Files/collection/bbold/Argo.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Sofas/argo/argo_1.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/argo/argo_2.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/argo/argo_3.jpg"),
+			require("../../../assets/images/collection/bbold/Sofas/argo/argo_4.jpg"),
+		]
+	},
 ]
 
 export default Basic_sofa;
--- src/Pages/Collections/BboldItems/BboldSofas.js
+++ src/Pages/Collections/BboldItems/BboldSofas.js
@@ -35,42 +35,7 @@ const Bbold_sofa = [
 			require("../../../assets/images/collection/bbold/Sofas/shetland/shetland_4.jpg"),
 		]
 	},
-	{
-		id: 4,
-		path: "sofa/lusso",
-		title: "Lusso",
-		file: require("../../../assets/Files/collection/bbold/Lusso.pdf"),
-		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_4.jpg"),
-		]
-	},
-	{
-		id: 5,
-		path: "sofa/richard",
-		title: "Richard",
-		file: require("../../../assets/Files/collection/bbold/Richard.pdf"),
-		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_4.jpg"),
-		]
-	},
-	{
-		id: 6,
-		path: "sofa/argo",
-		title: "Argo",
-		file: require("../../../assets/Files/collection/bbold/Argo.pdf"),
-		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_4.jpg"),
-		]
-	},
+	
 ]
 
 export default Bbold_sofa;
