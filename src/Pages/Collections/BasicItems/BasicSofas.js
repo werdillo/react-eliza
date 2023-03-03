816df5d69564e6cd5caffaa4f35538b89a43009c
werdillo <wediktivus@gmail.com>
2023-03-03 12:19:53 +0200

    fixed path, changed richard

--- src/Pages/Collections/BasicItems/BasicSofas.js
+++ src/Pages/Collections/BasicItems/BasicSofas.js
@@ -27,36 +27,36 @@ const Basic_sofa = [
 		id: 3,
 		path: "sofa/lusso",
 		title: "Lusso",
-		file: require("../../../assets/Files/collection/bbold/Lusso.pdf"),
+		file: require("../../../assets/Files/collection/basic/Lusso.pdf"),
 		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/lusso/lusso_4.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/lusso/lusso_1.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/lusso/lusso_2.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/lusso/lusso_3.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/lusso/lusso_4.jpg"),
 		]
 	},
 	{
 		id: 4,
 		path: "sofa/ričard",
 		title: "Ričard",
-		file: require("../../../assets/Files/collection/bbold/Richard.pdf"),
+		file: require("../../../assets/Files/collection/basic/Richard.pdf"),
 		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/richard/richard_4.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/richard/richard_1.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/richard/richard_2.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/richard/richard_3.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/richard/richard_4.jpg"),
 		]
 	},
 	{
 		id: 5,
 		path: "sofa/argo",
 		title: "Argo",
-		file: require("../../../assets/Files/collection/bbold/Argo.pdf"),
+		file: require("../../../assets/Files/collection/basic/Argo.pdf"),
 		photo: [
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_1.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_2.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_3.jpg"),
-			require("../../../assets/images/collection/bbold/Sofas/argo/argo_4.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/argo/argo_1.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/argo/argo_2.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/argo/argo_3.jpg"),
+			require("../../../assets/images/collection/basic/Sofas/argo/argo_4.jpg"),
 		]
 	},
 ]
