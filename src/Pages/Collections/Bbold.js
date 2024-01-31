3dffd474bde00f1b397d59fe8b914890376bbbbb
werdillo <55383654+werdillo@users.noreply.github.com>
2024-01-31 13:54:53 +0200

    asdded bed reefbreak

--- src/Pages/Collections/Bbold.js
+++ src/Pages/Collections/Bbold.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CollectionItem from './CollectionItem'
 import Bbold_sofa from './BboldItems/BboldSofas';
+import Bbold_beds from './BboldItems/BboldBeds';
 import { useTranslation } from 'react-i18next';
 
 export default function Bbold() {
@@ -10,7 +11,12 @@ export default function Bbold() {
       title: t("Pages.collection.sofas"),
       path: "sofa",
       data: Bbold_sofa,
-    }
+    },
+    {
+      title: t("Pages.collection.beds"),
+      path: "bed",
+      data: Bbold_beds,
+    },
   ]
   return (
     <CollectionItem data={data} collection="bbold" />
---
+++ src/Pages/Collections/BboldItems/BboldBeds.js
@@ -0,0 +1,19 @@
+const Bbold_bed = [
+	{
+		id: 1,
+		path: "bed/REEFBREAK",
+		title: "Reefbreak",
+		file: require("../../../assets/Files/collection/bbold/BedsFrames.pdf"),
+		photo: [
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_1.JPG"),
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_2.JPG"),
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_3.JPG"),
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_4.JPG"),
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_5.JPG"),
+			require("../../../assets/images/collection/bbold/Beds/reefbreak/reefbreak_6.JPG"),
+		]
+	},
+	
+]
+
+export default Bbold_bed;
